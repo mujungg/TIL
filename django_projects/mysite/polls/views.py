614cee9c73cf10ad482ce632772dad31e90bd975
choemujung <mnw0507@naver.com>
2022-01-12 23:40:35 +0900

    .

--- django_projects/mysite/polls/views.py
+++ django_projects/mysite/polls/views.py
@@ -19,8 +19,8 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 
 def vote(request, question_id):
@@ -29,10 +29,10 @@ def vote(request, question_id):
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
         return render(request, 'polls/detail.html', {
-            'question' : question,
+            'question': question,
             'error_message': "You didn't select a choice.",
         })
     else:
         selected_choice.votes += 1
         selected_choice.save()
-        return HttpResponseRedirect(reverse('poll:results', args=question.id))
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
