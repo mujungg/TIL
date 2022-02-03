fee444f28251803c514ba331de8f5001254c87d4
choemujung <mnw0507@naver.com>
2022-02-03 15:13:46 +0900

    알고리즘 33, 34번 풀이

--- algorithm/q_32.py
+++ algorithm/q_32.py
@@ -2,26 +2,51 @@ from typing import List
 
 
 class Solution:
-    def dfs(self, grid, i, j):
-        if i < 0 or i > len(grid) or \
-                j < 0 or j > len(grid[0]) or \
-                grid[i][j] != 1:
-            return
+    def numIslands(self, grid: List[List[str]]):
+        def dfs(i, j):
+            # 해당 인덱스가 땅이 아닌 경우 종료.
+            if i < 0 or j >= len(grid) or \
+                    j < 0 or j >= len(grid[0]) or \
+                    grid[i][j] != '1':
+                return
+            # 해당 위치가 육지인 경우 탐색 완료 표시 후 동서남북 검사
+            grid[i][j] = 0
 
-        grid[i][j] = 0
+            dfs(i + 1, j)
+            dfs(i - 1, j)
+            dfs(i, j + 1)
+            dfs(i, j - 1)
 
-        self.dfs(grid, i, j + 1)  # 동
-        self.dfs(grid, i, j - 1)  # 서
-        self.dfs(grid, i + 1, j)  # 남
-        self.dfs(grid, i - 1, j)  # 북
-
-    def numIslands(self, grid: List[List[str]]) -> int:
-        cnt = 0
-
+        count = 0
         for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if grid[i][j] == 1:
-                    self.dfs(grid, i, j)
-                    cnt += 1
+            for j in range(len(grid)):
+                dfs(i, j)
+                # 재귀 탈출하면 섬 하나 탐색한 것이므로 count++
+                count += 1
 
-        return cnt
+        return count
+
+# class Solution:
+#     def dfs(self, grid, i, j):
+#         if i < 0 or i > len(grid) or \
+#                 j < 0 or j > len(grid[0]) or \
+#                 grid[i][j] != 1:
+#             return
+#
+#         grid[i][j] = 0
+#
+#         self.dfs(grid, i, j + 1)  # 동
+#         self.dfs(grid, i, j - 1)  # 서
+#         self.dfs(grid, i + 1, j)  # 남
+#         self.dfs(grid, i - 1, j)  # 북
+#
+#     def numIslands(self, grid: List[List[str]]) -> int:
+#         cnt = 0
+#
+#         for i in range(len(grid)):
+#             for j in range(len(grid[0])):
+#                 if grid[i][j] == 1:
+#                     self.dfs(grid, i, j)
+#                     cnt += 1
+#
+#         return cnt
---
+++ algorithm/q_34.py
@@ -0,0 +1,31 @@
+from typing import List
+
+
+class Solution01:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        results = [d
+        prev_elements = []
+
+        def dfs(elements):
+            if len(elements) == 0:
+                results.append(prev_elements[:])
+
+            for e in elements:
+                next_elements = elements[:]
+                next_elements.remove(e)
+
+                prev_elements.append(e)
+                dfs(next_elements)
+                prev_elements.pop()
+
+        dfs(nums)
+        return results
+
+# import itertools
+# class Solution02:
+#     def permute(self, nums: List[int]) -> List[List[int]]:
+#         return list(itertools.permutations(nums))
+
+
+a = Solution01()
+print(a.permute([1, 2, 3]))
